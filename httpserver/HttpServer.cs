b1c2336850d5c487a2fe88b948e0af078d20137d
SuperStrandby <strandby@live.dk>
2014-09-29 12:49:43 +0200

    1a fixed

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -10,27 +10,39 @@ namespace httpserver
 {
     public class HttpServer
     {
-        //public static readonly int DefaultPort = 8888;
+        public static readonly int DefaultPort = 8888;
+
         public void StartServer()
         {
-            var serverSocket = new TcpListener(8080);
-            serverSocket.Start();
+            while (true)
+            {
+                var serverSocket = new TcpListener(DefaultPort);
+                serverSocket.Start();
 
-            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-            Console.WriteLine("Server activated");
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server activated");
 
-            Stream ns = connectionSocket.GetStream();
-            var sr = new StreamReader(ns);
-            var sw = new StreamWriter(ns) {AutoFlush = true};
+                Stream ns = connectionSocket.GetStream();
+                var sr = new StreamReader(ns);
+                var sw = new StreamWriter(ns) {AutoFlush = true};
 
 
-            sw.Write("http/1.0 200 Ok\r\n");
-            sw.Write("\r\n");
-            sw.Write("Hello world");
+                try
+                {
+                    
+                    sw.Write("HTTP/1.0 200 Ok\r\n");
+                    sw.Write("\r\n");
+                    sw.Write("Hello world");
+                }
+                finally
+                {
+                    ns.Close();
+                    connectionSocket.Close();
+                    serverSocket.Stop();
+                }
 
-            ns.Close();
-            connectionSocket.Close();
-            serverSocket.Stop();
+                
+            }
         }
     }
 }
---
+++ httpserver/HttpService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpserver
+{
+    internal class HttpService
+    {
+        private TcpClient connectionSocket;
+
+        public HttpService(TcpClient connectionSocket)
+        {
+            // TODO: Complete member initialization
+            this.connectionSocket = connectionSocket;
+        }
+
+        internal void SocketHandler()
+        {
+            Stream ns = connectionSocket.GetStream();
+            var sr = new StreamReader(ns);
+            var sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            while (!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine("Client: " + message);
+                string answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+
+            }
+            connectionSocket.Close();
+        }
+    }
+}
