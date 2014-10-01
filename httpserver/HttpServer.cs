ad76884a5ab422086da495f09fdf9afd7ef0bcb1
SuperStrandby <strandby@live.dk>
2014-10-01 21:23:48 +0200

    step 4 in progress

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace httpserver
@@ -12,27 +13,46 @@ namespace httpserver
     public class HttpServer
     {
         /// <summary>
-        /// Define our default port
+        /// Define default port
         /// </summary>
         public static readonly int DefaultPort = 8888;
         /// <summary>
-        /// The root folder of our files
+        /// The root folder for files
         /// </summary>
         private static readonly string _rootCatalog = @"C:\temp\";
+
+        private bool _serverActive;
 
         /// <summary>
         /// Method to start our webserver
         /// </summary>
         public void StartServer()
         {
-            while (true)
-            {
-                var serverSocket = new TcpListener(IPAddress.Parse("127.0.0.1"), DefaultPort);
-                serverSocket.Start();
+            _serverActive = true;
 
+            TcpListener serverSocket = new TcpListener(IPAddress.Parse("127.0.0.1"), DefaultPort);
+            serverSocket.Start();
+
+            List<Task> serverThreads = new List<Task>();
+
+            while (_serverActive)
+            {
                 //creates a connectionSocket by accepting the connection request from the client
                 TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-                Console.WriteLine("Server activated");
+                serverThreads.Add(Task.Run(()=>Connection(connectionSocket)));
+            }
+            serverSocket.Stop();
+            
+        }
+
+        public void _deactivateServer()
+        {
+            _serverActive = false;
+        }
+
+        public void Connection(TcpClient connectionSocket)
+        {
+                Console.WriteLine("Client connected on thread " + Thread.CurrentThread.GetHashCode());
                 Stream ns = connectionSocket.GetStream();
 
                 var sr = new StreamReader(ns);
@@ -52,7 +72,15 @@ namespace httpserver
                             throw new Exception("Document Empty");
                         }
 
-                        sw.WriteLine("You have requested: {0}", filename);
+                        
+
+                        sw.Write(
+                            "HTTP/1.0 200 Ok\r\n" +
+                            "\r\n" +
+                            "Server version 1.0\r\n" +
+                            "Date: {0}\r\n", DateTime.Now);
+
+                        sw.Write("You have requested: {0}" + "\r\n", filename);
                         Console.WriteLine("You have requested: " + words[1]);
                         FileStream fs = new FileStream(fullfilename, FileMode.Open, FileAccess.Read);
                         fs.CopyTo(sw.BaseStream);
@@ -68,19 +96,15 @@ namespace httpserver
                     {
                         ns.Close();
                         connectionSocket.Close();
-                        serverSocket.Stop();
                     }
 
                     
 
                     
-                    //sw.Write(
-                    //    "HTTP/1.0 200 Ok\r\n" +
-                    //    "\r\n" +
-                    //    " Hello World ");
+                    
 
                 
             }
-        }
-    }
+      }
 }
+
